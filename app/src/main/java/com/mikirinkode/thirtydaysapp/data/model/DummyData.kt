6a63a9738598d9af5e4d9488611a8977a3743fcb
Muhammad Wafa <muhammadwafa105@gmail.com>
2023-10-11 05:35:48 +0700

    add dummy images generated by AI

---
+++ app/src/main/java/com/mikirinkode/thirtydaysapp/data/model/DummyData.kt
@@ -0,0 +1,158 @@
+package com.mikirinkode.thirtydaysapp.data.model
+
+import com.mikirinkode.thirtydaysapp.R
+
+object DummyData {
+    fun getDailyActivityList() = listOf(
+        DailyActivity(
+            name = "Abstract Art",
+            description = "Create a piece of abstract art using geometric shapes (such as triangles, squares, circles) and bold, vibrant colors. The goal is to express emotion or energy through non-representational forms.",
+            illustrationId = R.drawable.abstract_art
+        ),
+        DailyActivity(
+            name = "Photo Manipulation",
+            description = "Take a photo (it could be one you've taken previously or a new one) and manipulate it creatively using photo editing software. Experiment with filters, blending modes, and effects.",
+            illustrationId = R.drawable.martial_arts
+        ),
+        DailyActivity(
+            name = "Nature Sketch",
+            description = "Go outdoors and sketch a natural scene or object. Focus on capturing the details and textures of plants, trees, flowers, or any aspect of nature that inspires you.",
+            illustrationId = R.drawable.nature_scetch
+        ),
+        DailyActivity(
+            name = "Collage Madness",
+            description = "Collect old magazines or newspapers and create a collage. Cut out interesting images, words, and textures, then arrange them into a visually appealing composition.",
+            illustrationId = R.drawable.collage_madnes
+        ),
+        DailyActivity(
+            name = "Gaming Logo",
+            description = "Create a Gaming Logo using a digital software like Adobe Illustrator or other.",
+            illustrationId = R.drawable.gaming_logo
+        ),
+        DailyActivity(
+            name = "Typography Design",
+            description = "Design a typographic poster featuring an inspiring or motivational quote. Experiment with different fonts, sizes, and layouts to make the text visually engaging.",
+            illustrationId = R.drawable.typography_design
+        ),
+        DailyActivity(
+            name = "Miniature World",
+            description = "Create a tiny diorama or scene using miniature figures, objects, or even paper cutouts. Tell a story or create a unique world in a small space.",
+            illustrationId = R.drawable.miniature_world
+        ),
+        DailyActivity(
+            name = "Color Explosion",
+            description = "Create a vibrant abstract artwork using every color in your palette. Experiment with color combinations, blending, and layering to make the colors pop.",
+            illustrationId = R.drawable.color_explosion
+        ),
+        DailyActivity(
+            name = "Digital Painting",
+            description = "Try your hand at digital painting using software like Adobe Photoshop or Procreate. Choose a subject or concept and create a digital artwork using brushes and layers.",
+            illustrationId = R.drawable.digital_painting
+        ),
+        DailyActivity(
+            name = "Recycled Art",
+            description = "Create art using only recycled materials you find at home. This could include cardboard, old newspapers, bottle caps, or any other discarded items.",
+            illustrationId = R.drawable.recycled_art
+        ),
+        DailyActivity(
+            name = "Ink Drawing",
+            description = "Create a drawing using ink paint.",
+            illustrationId = R.drawable.ink_drawing
+        ),
+        DailyActivity(
+            name = "Sculpture Challenge",
+            description = "Sculpt something using clay, paper, or any available material. You can create abstract sculptures or attempt to replicate a real object or creature.",
+            illustrationId = R.drawable.sculpture_challenge
+        ),
+        DailyActivity(
+            name = "Surreal Art",
+            description = "Create a surreal artwork that blurs the line between reality and imagination. Combine unrelated elements in a dreamlike composition.",
+            illustrationId = R.drawable.surreal_art
+        ),
+        DailyActivity(
+            name = "Pattern Design",
+            description = "Design a repeating pattern inspired by a theme of your choice, such as nature, geometry, or abstract shapes. This pattern can be used for textiles, wallpapers, or digital backgrounds.",
+            illustrationId = R.drawable.pattern_design
+        ),
+        DailyActivity(
+            name = "Impressionist Painting",
+            description = "Try your hand at an impressionist-style painting. Focus on capturing the play of light and color in a scene, using loose brushwork and visible strokes.",
+            illustrationId = R.drawable.impressionist_painting
+        ),
+        DailyActivity(
+            name = "Digital Animation",
+            description = "Create a short digital animation or GIF. You can use software like Adobe Animate or online animation tools to bring a simple scene to life.",
+            illustrationId = R.drawable.digital_animation
+        ),
+        DailyActivity(
+            name = "Origami",
+            description = "Fold a piece of paper into an intricate origami creation. You can find origami tutorials online for various skill levels.",
+            illustrationId = R.drawable.origami
+        ),
+        DailyActivity(
+            name = "Portraiture",
+            description = "Create a portrait of a family member, friend, or your favorite celebrity. Pay attention to capturing their likeness and personality.",
+            illustrationId = R.drawable.portraiture
+        ),
+        DailyActivity(
+            name = "Found Object Sculpture",
+            description = "Make a sculpture using objects you find around your home. Look for items like bottle caps, old keys, or pieces of broken jewelry to repurpose creatively.",
+            illustrationId = R.drawable.sculpt_anything_around
+        ),
+        DailyActivity(
+            name = "Abstract Photography",
+            description = "Experiment with abstract photography techniques. Focus on capturing patterns, textures, and unusual angles in everyday objects or scenes.",
+            illustrationId = R.drawable.abstract_photography
+        ),
+        DailyActivity(
+            name = "3D Modeling",
+            description = "Try your hand at 3D modeling using software like Blender or Tinkercad. Create a simple 3D object or scene.",
+            illustrationId = R.drawable.object_3d
+        ),
+        DailyActivity(
+            name = "Watercolor Painting",
+            description = "Paint a serene watercolor landscape. Watercolors are known for their translucent and fluid qualities, so experiment with washes and blending.",
+            illustrationId = R.drawable.watercolor_painting
+        ),
+        DailyActivity(
+            name = "Street Art",
+            description = "Create temporary street art using materials like chalk, washable paint, or sidewalk chalk. Share your art with your community or online.",
+            illustrationId = R.drawable.street_art
+        ),
+        DailyActivity(
+            name = "Papercut Art",
+            description = "Cut intricate designs from paper to create striking papercut art. Use a craft knife or scissors to create delicate and detailed compositions.",
+            illustrationId = R.drawable.papercut_art
+        ),
+        DailyActivity(
+            name = "Vector Avatar",
+            description = "Create an avatar of yourself or your friend with vector style using Adobe Illustrator.",
+            illustrationId = R.drawable.vector_person
+        ),
+        DailyActivity(
+            name = "Claymation",
+            description = "Make a short claymation animation. Use clay figures or objects to tell a simple story through stop-motion animation.",
+            illustrationId = R.drawable.claymation
+        ),
+        DailyActivity(
+            name = "Environmental Art",
+            description = "Create art in a natural setting using materials found in the environment, such as rocks, leaves, or sticks. Document your creation through photography.",
+            illustrationId = R.drawable.environtment_art
+        ),
+        DailyActivity(
+            name = "Minimalist Design",
+            description = "Design a minimalist poster or artwork with a limited color palette and simple, clean lines. Less is more in minimalist design.",
+            illustrationId = R.drawable.minimalist
+        ),
+        DailyActivity(
+            name = "Fantasy Illustration",
+            description = "Create an illustration of a fantastical scene or creature from your imagination. Let your creativity run wild in the realm of fantasy.",
+            illustrationId = R.drawable.fantasy
+        ),
+        DailyActivity(
+            name = "Free Choice",
+            description = "On the last day, choose any creative project that you've been wanting to work on. This is your opportunity to explore a personal passion or experiment with a new technique.",
+            illustrationId = R.drawable.free_choice
+        ),
+    )
+}
